cae435d8bfda3d3dd46c058fc05ed482c0a92a5d
ah0912784 <a.hansen354@gmail.com>
2024-02-22 22:54:49 -0600

    Civilization Seeder Woking. Throwing Coruppted path error when it reaches EOF

--- age_of_empires2_proj/laravel/app/Models/User.php
+++ age_of_empires2_proj/laravel/app/Models/User.php
@@ -8,10 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable 
+    {
     use HasApiTokens, HasFactory, Notifiable;
-
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
@@ -43,3 +43,6 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 }
+
+
+
